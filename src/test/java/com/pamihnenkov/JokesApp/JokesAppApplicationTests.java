324b15427aac4374fd11cb7a8962eba5eb05d902
zOdium <pamihnenkov@gmail.com>
2020-01-13 22:13:41 +0300

    Initial commit

---
+++ src/test/java/com/pamihnenkov/JokesApp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamihnenkov.JokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
